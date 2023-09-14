dddee34ce9d9eaa09f564e7091c9c9235d915584
imvenx <imvenx@proton.me>
2023-09-14 20:12:26 +0200

    init with small city

---
+++ Assets/Scripts/PlayerCarController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerCarController : MonoBehaviour
+{
+    // public ArcanePad Pad { get; private set; }
+    // private float speed = 0.1f;
+    // public void Initialize(ArcanePad pad)
+    // {
+    //     Pad = pad;
+
+    //     Pad.StartGetQuaternion();
+    //     Pad.OnGetQuaternion((GetQuaternionEvent e) =>
+    //     {
+    //         if (PlayerManager.isGamePaused) return;
+    //         transform.rotation = new Quaternion(e.x, e.y, e.z, e.w);
+    //     });
+    // }
+
+    // void FixedUpdate()
+    // {
+    //     transform.Translate(new Vector3(0, 0, speed));
+    // }
+
+}
